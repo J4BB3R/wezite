56db6c699bd2335f10c1548a50a3a7ceadf18ceb
Unknown <baptiste.chezaubernard@gmail.com>
2018-11-17 09:27:53 -0500

    Ajout de l'API et dessin des cartes

---
+++ app/src/main/java/ca/wezite/wezite/async/ParcoursAsyncCreator.java
@@ -0,0 +1,102 @@
+package ca.wezite.wezite.async;
+
+import android.os.AsyncTask;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.wezite.wezite.dev.InitData;
+import ca.wezite.wezite.model.Parcours;
+import ca.wezite.wezite.model.PointDinteret;
+import ca.wezite.wezite.model.PointParcours;
+import ca.wezite.wezite.utils.HttpUtils;
+
+public class ParcoursAsyncCreator extends AsyncTask<String, Void, Boolean> {
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+    }
+    private List<PointDinteret> listPointsDinterets;
+
+    private Parcours p;
+
+    public  void initParcours(){
+
+        p = new Parcours();
+        String key = "histoire";
+        p.setId(key);
+        p.setName("Aux originess historiques du Saguenay");
+
+        p.setListIdPointsInterets(new ArrayList<String>());
+        listPointsDinterets = InitData.initPointsDinterets();
+        for(PointDinteret point : listPointsDinterets){
+            p.getListIdPointsInterets().add(point.getId());
+        }
+
+        p.setListePoints(new ArrayList<PointParcours>());
+    }
+
+    @Override
+    protected Boolean doInBackground(String... params) {
+
+        initParcours();
+        for(int i=1;i<listPointsDinterets.size();i++){
+
+            PointDinteret pointDepart = listPointsDinterets.get(i-1);
+            PointDinteret pointArrivee = listPointsDinterets.get(i);
+            String url="https://api.openrouteservice.org/directions?api_key=5b3ce3597851110001cf6248a8274cc6c9994e8aaaa2480ae9cd9da5&coordinates="+pointDepart.getyCoord()+","+pointDepart.getxCoord()+"%7C"+pointArrivee.getyCoord()+","+pointArrivee.getxCoord()+"&profile=foot-walking&preference=recommended&geometry_format=polyline";
+            p.getListePoints().addAll(recupererPoints(url));
+        }
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DatabaseReference mDatabase =  FirebaseDatabase.getInstance().getReference();
+        DatabaseReference parcoursCloudEndPoint = mDatabase.child("parcours");
+        parcoursCloudEndPoint.removeValue();
+        parcoursCloudEndPoint.child(p.getId()).setValue(p);
+       return true;
+    }
+
+    protected List<PointParcours> recupererPoints(String url) {
+
+
+
+        try {
+            String data = HttpUtils.downloadUrl(url);
+            JSONObject jsono = new JSONObject(data);
+            JSONArray routes = jsono.getJSONArray("routes");
+            JSONArray geometry = routes.getJSONObject(0).getJSONArray("geometry");
+            final List<PointParcours> listePointsParcours = new ArrayList<>();
+            for(int i=0;i<geometry.length();i++){
+                JSONArray coordonnees = geometry.getJSONArray(i);
+                double yCoord = coordonnees.getDouble(0);
+                double xCoord = coordonnees.getDouble(1);
+                PointParcours p = new PointParcours(String.valueOf(xCoord), String.valueOf(yCoord));
+                listePointsParcours.add(p);
+            }
+
+            return listePointsParcours;
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return new ArrayList<>();
+    }
+
+    protected void onPostExecute(Boolean result) {
+
+    }
+
+}
