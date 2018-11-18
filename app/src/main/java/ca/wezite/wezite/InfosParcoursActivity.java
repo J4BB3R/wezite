3a3ead0817cbc81447d603ca15d574c12c98fe2f
Unknown <nicolascorronel@gmail.com>
2018-11-18 14:10:25 -0400

    Commit layout parcours info

---
+++ app/src/main/java/ca/wezite/wezite/InfosParcoursActivity.java
@@ -0,0 +1,13 @@
+package ca.wezite.wezite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfosParcoursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_infos_parcours);
+    }
+}
